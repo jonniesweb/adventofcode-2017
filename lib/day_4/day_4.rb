3fb8ba0116bb129e68d79e391d3029c1c42812fa
Jon Simpson <jon.simpson@shopify.com>
2017-12-16 23:17:27 -0500

    finish day 4 part 1

---
+++ lib/day_4/day_4.rb
@@ -0,0 +1,13 @@
+class Day4
+  def initialize(pass)
+    @pass = pass.split(' ')
+  end
+
+  def passphrase
+    @pass.each_with_object(Hash.new(0)) do |word, hash|
+      hash[word] += 1
+      return false if hash[word] > 1
+    end
+    true
+  end
+end
